5760a2a1d595d6f537be45870d5e5807156df289
Daniil Yurov <daniel.jurow@hotmail.com>
2016-08-12 21:31:27 +0300

    ConcreteLocalizer covered with comments and teststest

--- PROJECT_Web/src/test/java/com/daniilyurov/training/project/web/i18n/ConcreteLocalizerTest.java
+++ PROJECT_Web/src/test/java/com/daniilyurov/training/project/web/i18n/ConcreteLocalizerTest.java
@@ -1,6 +1,7 @@
 package com.daniilyurov.training.project.web.i18n;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.Locale;
@@ -9,9 +10,9 @@ import java.util.ResourceBundle;
 import static org.junit.Assert.*;
 
 
-public class ConcreteLocalizerTest {
+public class ConcreteLocalizerTest extends Mockito {
 
-    Locale supportedLocale = Locale.GERMAN;
+    Locale supportedGermanLocale = Locale.GERMAN;
     Locale unsupportedLocale = Locale.CHINESE;
     Locale defaultLocale = Locale.ENGLISH;
     String basename = "localization";
@@ -20,10 +21,10 @@ public class ConcreteLocalizerTest {
     @Test // tests: adjustLocale(Locale locale)
     public void supportedLocalePassed_returnsUnchanged() throws Exception {
         // execute
-        Locale adjustedLocale = new ConcreteLocalizer(basename).adjustLocale(supportedLocale);
+        Locale adjustedLocale = new ConcreteLocalizer(basename).adjustLocale(supportedGermanLocale);
 
         // verify
-        assertEquals(supportedLocale, adjustedLocale);
+        assertEquals(supportedGermanLocale, adjustedLocale);
     }
 
     @Test // tests: adjustLocale(Locale locale)
@@ -38,7 +39,7 @@ public class ConcreteLocalizerTest {
     @Test // tests: getBundle(Locale locale)
     public void supportedLocalePassed_returnsAdjustedResourceBundle() throws Exception {
         // execute
-        ResourceBundle bundle = new ConcreteLocalizer(basename).getBundle(supportedLocale);
+        ResourceBundle bundle = new ConcreteLocalizer(basename).getBundle(supportedGermanLocale);
 
         // verify
         assertNotNull(bundle);
@@ -69,23 +70,88 @@ public class ConcreteLocalizerTest {
         assertTrue(Arrays.equals(supportedLanguages, result));
     }
 
-    @Test // tests: getLocalName(T element, Locale locale)
-    public void testGetLocalName() throws Exception {
+    @Test(expected = NullPointerException.class) // tests: getLocalName(T element, Locale locale)
+    public void nullElementPassedForLocalName_throwsNullPointerException() throws Exception {
+        // execute
+        new ConcreteLocalizer(basename).getLocalName(null, supportedGermanLocale);
+    }
 
+    @Test(expected = NullPointerException.class) // tests: getLocalFirstName(T element, Locale locale)
+    public void nullElementPassedForLocalFirstName_throwsNullPointerException() throws Exception {
+        // execute
+        new ConcreteLocalizer(basename).getLocalFirstName(null, supportedGermanLocale);
+    }
+
+    @Test(expected = NullPointerException.class) // tests: getLocalLastName(T element, Locale locale)
+    public void nullElementPassedForLocalLastName_throwsNullPointerException() throws Exception {
+        // execute
+        new ConcreteLocalizer(basename).getLocalLastName(null, supportedGermanLocale);
+    }
+
+    @Test(expected = NullPointerException.class) // tests: getLocalDescription(T element, Locale locale)
+    public void nullElementPassedForLocalDescription_throwsNullPointerException() throws Exception {
+        // execute
+        new ConcreteLocalizer(basename).getLocalDescription(null, supportedGermanLocale);
+    }
+
+    @Test // tests: getLocalName(T element, Locale locale)
+    public void validElement_returnsLocalName() throws Exception {
+
+        // setup
+        NameLocalizable element = mock(NameLocalizable.class);
+        when(element.getDeName()).thenReturn("Erdkunde");
+
+        // execute
+        String result = new ConcreteLocalizer(basename)
+                .getLocalName(element, supportedGermanLocale);
+
+        // verify
+        assertEquals("Erdkunde", result);
     }
 
     @Test // tests: getLocalFirstName(T element, Locale locale)
-    public void testGetLocalFirstName() throws Exception {
+    public void validElement_returnsLocalFirstName() throws Exception {
+
+        // setup
+        FirstLastNameLocalizable element = mock(FirstLastNameLocalizable.class);
+        when(element.getLatinFirstName()).thenReturn("Schmidt");
+
+        // execute
+        String result = new ConcreteLocalizer(basename)
+                .getLocalFirstName(element, supportedGermanLocale);
+
+        // verify
+        assertEquals("Schmidt", result);
 
     }
 
     @Test // tests: getLocalLastName(T element, Locale locale)
-    public void testGetLocalLastName() throws Exception {
+    public void validElement_returnsLocalLastName() throws Exception {
+        // setup
+        Locale supportedRussianLocale = new Locale("ru");
+        FirstLastNameLocalizable element = mock(FirstLastNameLocalizable.class);
+        when(element.getCyrillicLastName()).thenReturn("Смирнов");
 
+        // execute
+        String result = new ConcreteLocalizer(basename)
+                .getLocalLastName(element, supportedRussianLocale);
+
+        // verify
+        assertEquals("Смирнов", result);
     }
 
     @Test // tests: getLocalDescription(T element, Locale locale)
-    public void testGetLocalDescription() throws Exception {
+    public void validElement_returnsLocalDescription() throws Exception {
 
+        // setup
+        DescriptionLocalizable element = mock(DescriptionLocalizable.class);
+        when(element.getDeDescription()).thenReturn("Beschreibung");
+
+        // execute
+        String result = new ConcreteLocalizer(basename)
+                .getLocalDescription(element, supportedGermanLocale);
+
+        // verify
+        assertEquals("Beschreibung", result);
     }
 }
